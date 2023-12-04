b9d4fe86da3fad2ed7710344be566ff1e3b85c38
Earl006 <earljoe06@gmail.com>
2023-12-04 15:08:31 -0800

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@
 def no_c(my_string):
     updated =''
     for i in my_string:
-        if i != 'c' or i != 'C':
+        if i != 'c' and i != 'C':
             updated += i
             return (updated)
