050371d8402a3f91acd85e8b8ef2c863a7e16c4c
Earl006 <earljoe06@gmail.com>
2023-12-04 15:23:45 -0800

    matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for row in matrix:
+            for element in row:
+                print("{:d}".format(element), end = " ")
+                print()
+            
+
