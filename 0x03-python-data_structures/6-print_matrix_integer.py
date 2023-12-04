dcba051af346dd09c3533ac7974c684964d5c1d1
Earl006 <earljoe06@gmail.com>
2023-12-04 15:31:06 -0800

    correct matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
-        for row in matrix:
-            for element in row:
-                print("{:d}".format(element), end = " ")
-            print()
+    for row in matrix:
+        for column in row:
+            if column == row[-1]:
+                print('{:d}'.format(column), end='')
+            else:
+                print('{:d}'.format(column), end=' ')
+        print()
